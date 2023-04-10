9b84a1ab56503a7ba5db18acd86e4a8fa83ce810
Roger Huba <rogerhuba@gmail.com>
2023-04-10 14:18:01 -0500

    fix over-write

--- api/aloha.py
+++ api/aloha.py
@@ -1,31 +1,25 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import requests
+import platform
 
+# https://serverless-fawn-iota.vercel.app/api/aloha?name=Roger
+# Hello Roger.  How are you?
+# Hello Stranger.  Stranger Danger!
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
-        dic = dict(query_string_list)
+        given_dictionary = dict(query_string_list)
+        name = given_dictionary.get('name')
 
-        if "word" in dic:
-            url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-            r = requests.get(url + dic["word"])
-            data = r.json()
-            definitions = []
-            for word_data in data:
-                definition = word_data["meanings"][0]["definitions"][0]["definition"]
-                definitions.append(definition)
-            message = str(definitions)
-
+        if name:
+            message = f"Hello {name}.  How are you?"
         else:
-            message = "Give me a word to define please"
-
+            message = "Hello Stranger.  Stranger Danger!"
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-
+        message += " " + platform.python_version()
         self.wfile.write(message.encode())
-
         return
--- api/define.py
+++ api/define.py
@@ -2,41 +2,30 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
-# https://serverless-fawn-iota.vercel.app/api/define?word="python"
-# "https://api.dictionaryapi.dev/api/v2/entries/en/python"
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
-        given_dictionary = dict(query_string_list)
-        word = given_dictionary.get('word')
+        dic = dict(query_string_list)
 
-        # base_url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-        # new_url = base_url + word
-        #
-        # req = requests.get(new_url)
-        # data = req.json
-        # definitions = []
-        # for word_data in data:
-        #     definition = word_data["meanings"][0]["definitions"][0]["definition"]
-        #     definitions.append(definition)
-        # message = str(definitions)
+        if "word" in dic:
+            url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+            r = requests.get(url + dic["word"])
+            data = r.json()
+            definitions = []
+            for word_data in data:
+                definition = word_data["meanings"][0]["definitions"][0]["definition"]
+                definitions.append(definition)
+            message = str(definitions)
+
+        else:
+            message = "Give me a word to define please"
 
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(word.encode())
-        return
 
-    # [{"word": "python", "phonetics": [
-    #     {"audio": "https://api.dictionaryapi.dev/media/pronunciations/en/python-au.mp3",
-    #      "sourceUrl": "https://commons.wikimedia.org/w/index.php?curid=79268748",
-    #      "license": {"name": "BY-SA 4.0", "url": "https://creativecommons.org/licenses/by-sa/4.0"}},
-    #     {"text": "/ˈpaɪθən/", "audio": ""}, {"text": "/ˈpaɪθɑːn/", "audio": ""}], "meanings": [
-    #     {"partOfSpeech": "noun",
-    #      "definitions": [{"definition": "A type of large constricting snake.", "synonyms": [], "antonyms": []},
-    #                      {"definition": "Penis", "synonyms": [], "antonyms": []}], "synonyms": [], "antonyms": []}],
-    #   "license": {"name": "CC BY-SA 3.0", "url": "https://creativecommons.org/licenses/by-sa/3.0"},
-    #   "sourceUrls": ["https://en.wiktionary.org/wiki/python"]}]
+        self.wfile.write(message.encode())
+
+        return
