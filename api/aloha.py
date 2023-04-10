e3f3a2010248bcb53a418dd0d1e4ea4f6633e3ef
Roger Huba <rogerhuba@gmail.com>
2023-04-10 13:13:32 -0500

    add headers

--- api/aloha.py
+++ api/aloha.py
@@ -8,5 +8,8 @@ import platform
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
         self.wfile.write(s.encode())
         return
