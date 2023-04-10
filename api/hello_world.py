cdaf2dc3eee049c8857d95e0f7f10db554486de3
Roger Huba <rogerhuba@gmail.com>
2023-04-10 12:48:15 -0500

    add encode method

--- api/hello_world.py
+++ api/hello_world.py
@@ -6,5 +6,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         message = "Hello World, Roger's Serverless Method"
-        self.wfile.write(message)
+        self.wfile.write(message.encode())
         return
