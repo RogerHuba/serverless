517b152866f1eeb2b4ca15b7c7f8fb70266a3fff
Roger Huba <rogerhuba@gmail.com>
2023-04-10 13:15:35 -0500

    add split

--- api/aloha.py
+++ api/aloha.py
@@ -8,8 +8,13 @@ import platform
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
+        url_components = parse.urlsplit(s)
+
+
+
+
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(s.encode())
+        self.wfile.write(url_components.encode())
         return
