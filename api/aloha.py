64ed479c576508fac72cea90582dbaa79fa3b5c5
Roger Huba <rogerhuba@gmail.com>
2023-04-10 13:27:00 -0500

    add version

--- api/aloha.py
+++ api/aloha.py
@@ -20,5 +20,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
+        message += message + " " + platform.python_version()
         self.wfile.write(message.encode())
         return
